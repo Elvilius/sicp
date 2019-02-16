3ded3c035673d314ed6a686df90d0a13d3230573
Elvilius <vik.sergienko@mail.ru>
2019-02-17 00:10:29 +0300

    update project

---
+++ chapter01/cbrt.scm
@@ -0,0 +1,25 @@
+
+(define (square x) (* x x))
+
+(define (cbrt-iter guess x)
+  (if (eng? guess x)
+      guess
+    (cbrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+(aveger (/ x (square guess)) (* 2 guess)))
+
+(define (aveger x y )
+  (/ ( + x y )3))
+
+(define (cube x)(* x x x))
+
+(define (eng? guess x)
+(< (abs(-(cube guess) x)) 0.00001))
+
+
+(define (cbrt x)
+  (cbrt-iter 1.0 x))
+
+
+(cbrt 27)
